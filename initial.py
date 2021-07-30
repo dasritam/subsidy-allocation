7f21de9d0cda777bcf260fd5091addee43071f49
Ritam Das <66792184+dasritam@users.noreply.github.com>
2021-07-30 19:11:53 +0530

    Initializing the population array

---
+++ initial.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def gen_popn(N=10000, c_min=10, a=5):
+    c = np.random.poisson(2*np.log(10), size=(N, 1)) * c_min + c_min
+    u = np.random.exponential(0.4, size=(N, 1)) * \
+        c_min**2  # np.zeros(shape=(N, 1))
+    mu = a*u * np.exp(np.divide(-u, a))
+    mu[mu < a] = a**2/np.e
+    beta = np.random.uniform(np.zeros((N, 1)), c/mu)
+    psi = beta*mu/c
+    popn = np.column_stack((u, c, mu, beta, psi))
+    return popn
